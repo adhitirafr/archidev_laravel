34a6f71f8563d3851843a70d445346afb939c3de
adhitirafr <adhitirafr@gmail.com>
2021-08-18 15:01:00 +0700

    init

---
+++ database/migrations/2021_08_18_035213_create_portofolios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortofoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portofolios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('year')->nullable();
+            $table->string('short_description')->nullable();
+            $table->string('theme')->nullable();
+            $table->string('softwares')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portofolios');
+    }
+}
